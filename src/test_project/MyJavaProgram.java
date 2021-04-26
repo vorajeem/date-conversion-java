454d6923be4900b9cd172dfb387c80569dabd0c6
vorajeem <vorajeem@gmail.com>
2021-04-26 12:24:28 +0200

    Added all the Java files

---
+++ src/test_project/MyJavaProgram.java
@@ -0,0 +1,36 @@
+package test_project;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MyJavaProgram {
+
+	public static void main(String[] args) throws ParseException {
+//		Date startDate = simpleDateFormat.parse("20210419");
+		
+		// TODO Auto-generated method stub
+		
+		// the format you'd like to display your date as.
+		String pattern ="yyyyMMdd";
+		// this formats the date to look how you'd want it to look
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+		
+
+		//Get date for 2 day prior to currentDate.
+		long SINGLE_DAY_IN_MS = 1000 * 60 * 60 *24;
+		long nDaysToGoBack = 2;		
+		//create 'startDate' variable from milliseconds as an input
+		Date startDate = new Date(System.currentTimeMillis()- (nDaysToGoBack*SINGLE_DAY_IN_MS));
+		
+		//	Set 'endDate' as your current date.
+		Date endDate = new Date(System.currentTimeMillis());
+		System.out.println();
+		System.out.println("Start Date: "+ simpleDateFormat.format(startDate));
+		System.out.println("End Date: "+ simpleDateFormat.format(endDate));
+	}
+
+}
